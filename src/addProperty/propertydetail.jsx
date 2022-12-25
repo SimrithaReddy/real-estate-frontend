b90ac4ff1e13ab45eae4cc316928f2a94109fbd6
SimrithaReddy <95024988+SimrithaReddy@users.noreply.github.com>
2022-12-25 19:42:29 +0530

    Update propertydetail.jsx

--- src/addProperty/propertydetail.jsx
+++ src/addProperty/propertydetail.jsx
@@ -2,14 +2,14 @@ import "../styles/form.css"
 
 const PropertDetail = ({formData, setFormData}) => {
 
-    console.log(formData);
+   // console.log(formData);
     const handleFormData = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setFormData(previousData => {
             return {...previousData, [name]: value}
         });
-        console.log(formData);
+        //console.log(formData);
     }
 
     return (
